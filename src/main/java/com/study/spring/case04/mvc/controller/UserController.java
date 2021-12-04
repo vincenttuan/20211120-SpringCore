9c75085138543de9854a9fbaac3112614e2d0faa
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-04 16:02:17 +0800

    Add files via upload

--- src/main/java/com/study/spring/case04/mvc/controller/UserController.java
+++ src/main/java/com/study/spring/case04/mvc/controller/UserController.java
@@ -1,10 +1,29 @@
 package com.study.spring.case04.mvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.study.spring.case04.mvc.entity.User;
+import com.study.spring.case04.mvc.service.UserService;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
 	public UserController() {
 		System.out.println("UserController");
 	}
+	
+	public void create(User user) {
+		userService.add(user);
+	}
+	
+	public List<User> queryUsers() {
+		return userService.queryUsers();
+	}
+	
 }
