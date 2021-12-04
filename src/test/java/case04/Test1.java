5aff91e131ba06f38ff934af89d97ffe9cfbbfef
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-04 16:16:49 +0800

    Update Test1.java

--- src/test/java/case04/Test1.java
+++ src/test/java/case04/Test1.java
@@ -8,6 +8,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new  ClassPathXmlApplicationContext("applicationContext4.xml");
 		
+		UserController userController = ctx.getBean(UserController.class);
+		System.out.println(userController);
+		userController.create(new User("John", 20));
+		System.out.println(userController.queryUsers());
+		
 		((ClassPathXmlApplicationContext)ctx).close();
 	}
 	
